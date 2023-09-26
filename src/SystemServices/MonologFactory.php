10c00972074e0c279057bdcc5cb8d7674cfa72fe
lucasqc04 <lucasqcampos9@gmail.com>
2023-09-25 22:17:33 -0300

    request com monolog e slim no php

---
+++ src/SystemServices/MonologFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\SystemServices;
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+class MonologFactory{
+
+    private static $logger;
+
+    public static function getInstance(){
+
+if(self::$logger == null){
+
+    self::$logger = new Logger("MeuApp");
+
+ self::$logger -> pushHandler(new StreamHandler("meuslogs.log", Level::Debug));
+ 
+return self::$logger ->error('Ocorreu um erro: ');
+
+}else{
+
+    return self::$logger;
+    
+}
+
+    }
+
+
+}
